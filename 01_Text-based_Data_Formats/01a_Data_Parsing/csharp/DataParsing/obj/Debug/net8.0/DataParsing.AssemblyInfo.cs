ea95ffff203130d76924d4408de015214550b57d
OliverRoat <oliver.roat@gmail.com>
2025-01-31 12:20:34 +0100

    plz let this work

--- 01_Text-based_Data_Formats/01a_Data_Parsing/csharp/DataParsing/obj/Debug/net8.0/DataParsing.AssemblyInfo.cs
+++ 01_Text-based_Data_Formats/01a_Data_Parsing/csharp/DataParsing/obj/Debug/net8.0/DataParsing.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataParsing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6834373037e282ef832c26d0553223937b11df08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4545df8401e8fc11f4abeedaf0720babcceb3bb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataParsing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataParsing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
