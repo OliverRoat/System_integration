534ca23da62ec35e8c8e0f4a1056b97fabf186fa
OliverRoat <oliver.roat@gmail.com>
2025-03-10 12:36:50 +0100

    added websockets in python

--- 08_server-sent_events/python/main.py
+++ 08_server-sent_events/python/main.py
@@ -3,6 +3,8 @@ from fastapi.responses import StreamingResponse
 from fastapi.staticfiles import StaticFiles
 import time
 import asyncio
+import random
+import string
 
 app = FastAPI()
 
@@ -17,6 +19,18 @@ async def synchronize_time(request: Request):
                 break
             time_str = time.strftime("%Y-%m-%d %H:%M:%S")
             yield f"data: {time_str}\n\n"
+            await asyncio.sleep(1)
+
+    return StreamingResponse(event_generator(), media_type="text/event-stream")
+
+@app.get("/randomstring")
+async def random_string(request: Request):
+    async def event_generator():
+        while True:
+            if await request.is_disconnected():
+                break
+            random_str = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+            yield f"data: {random_str}\n\n"
             await asyncio.sleep(1)
 
     return StreamingResponse(event_generator(), media_type="text/event-stream")
